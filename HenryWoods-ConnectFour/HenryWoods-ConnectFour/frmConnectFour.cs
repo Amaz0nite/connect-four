f83b018967d833bfba53534f16957ad72e80660a
unknown <henrywoods022@gmail.com>
2019-05-15 23:53:05 +1000

    Created graphics background, Allowed players to take in turns, win detection

---
+++ HenryWoods-ConnectFour/HenryWoods-ConnectFour/frmConnectFour.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HenryWoods_ConnectFour
+{
+    public partial class frmConnectFour : Form
+    {
+        Cell[,] CellArray = new Cell[7, 6];
+        Boolean PlayerOnesTurn = true;
+
+        public frmConnectFour()
+        {
+            InitializeComponent();
+        }
+
+        // When the user click the screen, The system should get their coordinates and
+        // Check them against the CellArray.
+        private void ScreenClick(object sender, MouseEventArgs e)
+        {
+            String X = e.X.ToString();
+            String Y = e.Y.ToString();
+            Console.WriteLine("THE COORDS ARE: " + X + " , " + Y);
+            //MessageBox.Show("THE COORDS ARE: " + X + " , " + Y);
+            // Check that the player clicked on the connect four game area.
+            if (e.X < 700 && e.Y < 600)
+            {
+                //MessageBox.Show("INSIDE");
+                int Coord = 100;
+                int Column = 0;
+                int Row = 0;
+                // Get the Column Index
+                for (int i = 0; i != 7; i++)
+                {
+                    if(e.X < Coord)
+                    {
+                        Column = i;
+                        break;
+                    }
+                    else { Coord = Coord + 100; }
+                }
+                // Get the Row index
+                Coord = 100;
+                for (int i = 0; i != 6; i++)
+                {
+                    if (e.Y < Coord)
+                    {
+                        Row = i;
+                        break;
+                    }
+                    else { Coord = Coord + 100; }
+                }
+                //MessageBox.Show("" + CellArray.GetValue(Column,Row));
+                Graphics g = CreateGraphics();
+                //Creating 2 separate colours for each player
+                SolidBrush PlayerColor;
+                SolidBrush Red = new SolidBrush(Color.Red);
+                SolidBrush Blue = new SolidBrush(Color.Blue);
+                if(PlayerOnesTurn == true) { PlayerColor = Red; } else { PlayerColor = Blue; }
+                
+                //Draws circle representing the players move
+                g.FillEllipse(PlayerColor, new Rectangle(CellArray[Column,Row].getCoordX()-94, CellArray[Column, Row].getCoordY()- 95, 88, 88));
+
+
+                // Set the occupied space to the designated player
+                if (PlayerOnesTurn == true) { CellArray[Column, Row].setPlayerOccupied(1); CheckWinner(1); } else { CellArray[Column, Row].setPlayerOccupied(2); CheckWinner(2); }
+
+                //Changes the players turn
+                if (PlayerOnesTurn == true) { PlayerOnesTurn = false; } else { PlayerOnesTurn = true; }
+
+
+                
+
+            }
+            else
+            {
+                MessageBox.Show("OUTSIDE");
+            }
+
+        }
+        
+        public bool CheckWinner(byte Player)     
+        {
+            int WinCounter = 0;
+
+            //Checks for winner Vertically 
+            for (int x = 0; x != 7; x++)
+            {
+                WinCounter = 0;
+                for (int y = 0; y != 6; y++)
+                {
+                    if (CellArray[x, y].getPlayerOccupied().Equals(Player))
+                    {
+                        WinCounter += 1;
+                        if (WinCounter == 4) { MessageBox.Show("Player" + Player + " WINS"); }
+                    }
+                    else if(! CellArray[x, y].getPlayerOccupied().Equals(Player)){ WinCounter = 0; }
+                }
+            }
+            //CHECK HORIZONTAL
+            for (int x = 0; x != 6; x++)
+            {
+                WinCounter = 0;
+                for (int y = 0; y != 7; y++)
+                {
+                    if (CellArray[y, x].getPlayerOccupied().Equals(Player))
+                    {
+                        WinCounter += 1;
+                        if (WinCounter == 4) { MessageBox.Show("Player" + Player + " WINS"); }
+                    }
+                    else if (!CellArray[y, x].getPlayerOccupied().Equals(Player)) { WinCounter = 0; }
+                }
+            }
+
+
+            // Detects Diagonal down right win
+            // The first two loops will just make sure you check every cell
+            // The third loop is the one that checks for the diagonal win
+            int Row;
+            WinCounter = 0;
+            for (int StartingRow = 0; StartingRow != 6; StartingRow++)
+            {
+                Row = StartingRow;
+                for (int StartingColumn = 0; StartingColumn != 7; StartingColumn++)
+                {
+                 
+                    for (int Column = StartingColumn; Column != 7;)
+                    {
+                        if (CellArray[Column, Row].getPlayerOccupied().Equals(Player))
+                        {
+                            WinCounter += 1;
+                            if (WinCounter == 4) { MessageBox.Show("Player" + Player + " WINS"); }
+                        }
+                        if (Column == 6 || Row == 5) { WinCounter = 0; break; } else { Row += 1;Column += 1; }
+                    }
+                    Row = StartingRow;
+                    if (WinCounter == 4) { break; }
+                }
+                if (WinCounter == 4) { break; }
+            }
+
+            // Detects Diagonal Left win
+            // Its a little modification of the previous diagonal loop     
+            WinCounter = 0;
+            for (int StartingRow = 0; StartingRow != 6; StartingRow++)
+            {
+                Row = StartingRow;
+                for (int StartingColumn = 0; StartingColumn != 7; StartingColumn++)
+                {
+
+                    for (int Column = StartingColumn; Column != 7;)
+                    {
+                        if (CellArray[Column, Row].getPlayerOccupied().Equals(Player))
+                        {
+                            WinCounter += 1;
+                            if (WinCounter == 4) { MessageBox.Show("Player" + Player + " WINS"); }
+                        }
+                        if (Column == 0 || Row == 5) { WinCounter = 0; break; } else { Row += 1; Column -= 1; }
+                    }
+                    Row = StartingRow;
+                    if (WinCounter == 4) { break; }
+                }
+                if (WinCounter == 4) { break; }
+            }
+
+
+
+            return true;
+
+            
+        }
+
+        private void frmConnectFour_Load(object sender, EventArgs e)
+        {
+            //initializing the starting cell position
+            int RowCoord = 100;
+            int ColumnCoord = 100;
+
+            for (int y = 0; y != 7; y++)
+            {
+                for (int x = 0; x != 6; x++)
+                {
+                    CellArray[y, x] = new Cell(y, x, RowCoord, ColumnCoord);
+                    //Adding a new Cell object at column,row position
+                    //CellArray.Add(new Cell(y,x,RowCoord,ColumnCoord));
+                    RowCoord = RowCoord + 100;
+                    //System.Diagnostics.Debug.WriteLine(CellArray.IndexOf(2));
+
+                }
+                //incrementing the column coord
+                ColumnCoord += 100;
+                // Reset Row coord
+                RowCoord = 100;
+            }
+            //Prints each objects ToString method to the console
+
+            for (int y = 0; y != 7; y++)
+            {
+                for (int x = 0; x != 6; x++)
+                {
+                    System.Diagnostics.Debug.WriteLine(CellArray[y, x]);
+                }
+
+            }
+
+
+
+
+
+        }
+
+
+
+        private void frmConnectFour_Paint(object sender, PaintEventArgs e)
+        {
+            int RowCoord = 0;
+            int ColumnCoord = 0;
+
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    System.Drawing.Brush myBrush = new SolidBrush(Color.Black);
+                    System.Drawing.Pen myPen = new System.Drawing.Pen(myBrush, 10);
+                    System.Drawing.Graphics formGraphics;
+                    formGraphics = this.CreateGraphics();
+                    formGraphics.DrawRectangle(myPen, new Rectangle(RowCoord, ColumnCoord, 100, 100));
+                    myPen.Dispose();
+                    formGraphics.Dispose();
+                    RowCoord = RowCoord + 100;
+
+
+                }
+                ColumnCoord = ColumnCoord + 100;
+                RowCoord = 0;
+            }
+        }
+    }
+}
+            
+            
+        
+    
+
+
---
+++ HenryWoods-ConnectFour/HenryWoods-ConnectFour/frmMainMenu.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace HenryWoods_ConnectFour
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameManualToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(217, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 165);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect Four \r\n         by \r\nHenry Woods";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Green;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStart.Location = new System.Drawing.Point(266, 245);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(204, 155);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.DarkRed;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExit.Location = new System.Drawing.Point(266, 417);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(204, 75);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameManualToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // gameManualToolStripMenuItem
+            // 
+            this.gameManualToolStripMenuItem.Name = "gameManualToolStripMenuItem";
+            this.gameManualToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.gameManualToolStripMenuItem.Text = "Game Manual";
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMainMenu";
+            this.Load += new System.EventHandler(this.frmMainMenu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameManualToolStripMenuItem;
+    }
+}
